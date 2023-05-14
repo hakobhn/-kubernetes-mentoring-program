f96b6ea1818f1719043051fa8f941205363b6899
Hakob_Hakobyan3 <hakob.hakobyan.-nd@disney.com>
2023-05-14 23:42:08 +0400

    Adding deployment configuration under k8s

--- 2-k8s-overview/task/users-ms/postgres/init.sql
+++ 2-k8s-overview/task/users-ms/postgres/init.sql
@@ -2,7 +2,7 @@
 
 CREATE TABLE IF NOT EXISTS users
 (
-    id bigint NOT NULL AUTO_INCREMENT,
+    id serial not null primary key,
     username character varying(255) NOT NULL,
     number_of_posts integer NOT NULL,
     created_at timestamp without time zone,
